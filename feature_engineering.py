831a39e23a007fbae9baf6c5632164361903ff1e
Zijun Yao <393314766@qq.com>
2018-04-03 20:28:17 +0200

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -5,3 +5,12 @@ df = df.merge(gp, on=['ip', 'app', 'channel'], how='left')
 del gp
 gc.collect()
 print( df.info() )
+
+
+print('group by: ip_app_channel')
+gp = df[['device','hour']].groupby(by=['device'])[['hour']].count().reset_index().rename(index=str, columns={'hour': 'nDiv_h'})
+df = df.merge(gp, on=['device'], how='left')
+del gp
+df['nDiv_h'] = df['nDiv_h'].astype('uint16')
+gc.collect()
+print( df.info() )
